032c496471b863ba4c623ef37d6a24d5a02c4f35
RayXubuntu <pranshuchawla19@gmail.com>
2024-08-22 14:35:30 -0400

    bu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const userForm = document.getElementById('userForm');
+    const dutyForm = document.getElementById('dutyForm');
+    const assignmentForm = document.getElementById('assignmentForm');
+    const userList = document.getElementById('userList');
+    const dutyList = document.getElementById('dutyList');
+    const userSelect = document.getElementById('userSelect');
+    const dutySelect = document.getElementById('dutySelect');
+
+    let users = [];
+    let duties = [];
+});
