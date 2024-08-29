3350de75193856af56d1cd139e0bbcd7945c0c05
Ray <pranshuchawla19@gmail.com>
2024-08-29 12:30:32 -0400

    routing and app.html intro

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development', // Set the mode to 'development'
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      filename: 'index.html',
-      inject: 'body',
-    }),
-  ],
   module: {
     rules: [
       {
@@ -24,14 +19,33 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      }
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/app.html',
+      filename: 'app.html'
+    })
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
     compress: true,
     port: 9000,
-    open: true, // This will open the browser automatically
-  },
+    open: true,
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/app$/, to: '/app.html' }
+      ]
+    }
+  }
 };
